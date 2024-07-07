2170f005f3801bfc9bed828184e2632aea3e144d
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2024-07-07 11:19:01 +0700

    Add Controller for Coupon

---
+++ HyperSAPI/APP.API/Controllers/CouponController.cs
@@ -0,0 +1,31 @@
+﻿using APP.Bus.Repository.BLLs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private CouponBLL _BLL;
+
+        [HttpPost]
+        public ActionResult GetListCoupon([FromBody] dynamic request)
+        {
+            var products = _BLL.GetListCoupon(request);
+            if (products.ObjectReturn?.Data == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCoupon([FromBody] dynamic request)
+        {
+            var products = _BLL.UpdateCoupon(request);
+            return Ok(products);
+        }
+    }
+}
