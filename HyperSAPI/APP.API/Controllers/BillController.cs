ca1f43516fb87c9c35aa05df64c9be91887eb3a1
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2024-07-10 20:17:05 +0700

    Add role customer to use Updatebill

--- HyperSAPI/APP.API/Controllers/BillController.cs
+++ HyperSAPI/APP.API/Controllers/BillController.cs
@@ -66,7 +66,7 @@ namespace APP.API.Controllers
             return NotFound();
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin,Customer")]
         [HttpPost]
         public ActionResult UpdateBill([FromBody] dynamic request)
         {
