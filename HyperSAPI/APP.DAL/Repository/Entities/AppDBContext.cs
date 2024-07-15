421e8af0f8305558283bb142110a2f1680e33309
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2024-07-15 11:04:31 +0700

    Remove DBlink

--- HyperSAPI/APP.DAL/Repository/Entities/AppDBContext.cs
+++ HyperSAPI/APP.DAL/Repository/Entities/AppDBContext.cs
@@ -57,10 +57,6 @@ public partial class AppDBContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
-        if (connectionString == null)
-        {
-            connectionString = "Server=hyperssql-cakhosolo2003-325a.e.aivencloud.com;Port=17997;Database=defaultdb;User=avnadmin;Password=AVNS_EBxOtAQ6lHdDe2fbQEh;SslMode=Required;";
-        }
         optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
